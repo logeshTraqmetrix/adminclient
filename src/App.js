509a5920b67579c4a18d3f0544433cde06491553
Logesh Traq <logesh.mg@traqmetrix.com>
2024-07-09 19:20:42 +0530

    new update 2

--- src/App.js
+++ src/App.js
@@ -46,7 +46,52 @@
 
 
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import io from 'socket.io-client';
+// import MapComponent from './MapComponent';
+
+// const App = () => {
+//   const [locations, setLocations] = useState([]);
+//   const socket = useRef(null);
+
+//   useEffect(() => {
+//     socket.current = io('https://backendlive-hpko.onrender.com');
+    
+//     socket.current.on('locationUpdate', (data) => {
+//       setLocations((prevLocations) => {
+//         const index = prevLocations.findIndex((location) => location.id === data.id);
+//         if (index !== -1) {
+//           const newLocations = [...prevLocations];
+//           newLocations[index] = data;
+//           console.log('admin view location', newLocations)
+//           return newLocations;
+//         } else {
+//           return [...prevLocations, data];
+//         }
+//       });
+//     });
+
+//     return () => {
+//       socket.current.off('locationUpdate');
+//       socket.current.disconnect();
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Delivery Boys Locations</h1>
+//       <MapComponent locations={locations} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import MapComponent from './MapComponent';
 
